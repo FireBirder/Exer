0f0b8351db20a4616b81353e7e6b8ad4a09e8976
Jack <Jack-PC>
2019-03-20 18:33:22 +0800

    Modified Hello.java file

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
     public static void main(String[] args){
-        System.out.println("www.qq.com");
+        System.out.println("Happy New Year");
+        System.out.println("Happy Myself");
     }
 }
