09d59a38be017c7eccd1b760886a280fcc47e2f1
Jack <Jack-PC>
2019-03-21 08:13:26 +0800

    add static method print()

--- Hello.java
+++ Hello.java
@@ -1,6 +1,11 @@
 public class Hello{
+	public static void print(){
+		Sytem.out.println("test conflict");
+	}
     public static void main(String[] args){
         System.out.println("Happy New Year");
         System.out.println("Happy Myself");
+		print();
     }
+	
 }
